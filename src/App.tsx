a7e60367cb3e537c24c1fcc35ffb2f94083a0bcb
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-01-11 13:11:28 -0300

    login validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
 import Routes from "./routes";
 
 import GlobalStyle from "./styles/global";
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
-
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import getValidationErrors from "../../utils/getValidationErrors";
+
 import logoImg from "../../assets/logo.svg";
 
 import Input from "../../components/Input";
@@ -16,16 +19,35 @@ interface LoginData {
 }
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: LoginData): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: LoginData): Promise<void> => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("Email não preenchido")
+          .email("Digite um email válido"),
+        password: Yup.string().required("Senha não preenchida"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
 
           <Input icon={FiMail} name="email" placeholder="E-mail" />
