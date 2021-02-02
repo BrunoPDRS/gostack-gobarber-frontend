cef8028a733b40edce5840f6f06f7aa5f80ea49b
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-01 22:48:06 -0300

    handling authenticated Routing and Redirecting

---
+++ src/routes/Route.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Redirect,
+  Route as ReactRoute,
+  RouteProps as ReactRouteProps,
+} from "react-router-dom";
+
+import { useAuth } from "../hooks/auth";
+
+interface RouteProps extends ReactRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+  const isSigned = !!user;
+  return (
+    <ReactRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === isSigned ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? "/" : "/dashboard",
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
