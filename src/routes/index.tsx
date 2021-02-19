c548d7a6dc28a3f30a0126263ef7a021fba72d6b
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-18 21:55:56 -0300

    create reset password interface

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
 import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
 
 import Route from "./Route";
 
@@ -13,6 +14,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
     <Route path="/register" component={SignUp} />
     <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
